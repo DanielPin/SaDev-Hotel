596dd728b8edf9fd3818e3fdbcceab515293a3ab
Daniel Pinheiro <daniel.pin@outlook.com>
2022-03-11 00:14:01 -0300

    feat: projeto inicial

---
+++ src/main/java/br/com/hotel/sadev/SadevApplication.java
@@ -0,0 +1,13 @@
+package br.com.hotel.sadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadevApplication.class, args);
+	}
+
+}
